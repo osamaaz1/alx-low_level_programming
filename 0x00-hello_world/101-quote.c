3cd6b92931a8f0d359f8a8058475d500cdb11a9d
Osama <Osama.az1.os@gmail.com>
2023-07-14 10:45:58 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program to print without using print!
+ *
+ * Return: 1
+ *
+ * **/
+
+int main(void)
+{	char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, m, 59);
+	return (1);
+}
