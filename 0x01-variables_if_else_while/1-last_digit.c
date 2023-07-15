c6516f95bffc26f5b18d791c794cf589ae72bcf3
Osama <Osama.az1.os@gmail.com>
2023-07-15 00:51:47 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,12 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit = abs(n % 10);
+	
+	last_digit = abs(n % 10);
 
 	printf("Last digit of %d is %d", n, last_digit);
 	if (last_digit > 5)
