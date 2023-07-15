18f2b6ef8603d495779e1717d1e428a8c1726f9d
Osama <Osama.az1.os@gmail.com>
2023-07-15 02:42:27 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry part
+ *
+ * Description: print alphabet lowercase.
+ *
+ * Return: always 0 (success).
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	char c = 'a';
+
+	while (num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+	while (c <= f)
+	{
+		putchar(c);
+		c++;
+	}
+	printf("\n");
+
+	return (0);
+}
