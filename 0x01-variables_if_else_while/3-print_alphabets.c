06216f55fe65c43ecb9687dfa8f67c88b19603e9
Osama <Osama.az1.os@gmail.com>
2023-07-15 01:36:24 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry part
+ *
+ * Description: print alphabet lowercase.
+ *
+ * Return: always 0 (success).
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		if (c == z)
+		{
+			c = 'A';
+			continue;
+		}
+		c++
+	}
+
+	putchar('\n');
+	return (0);
+}
