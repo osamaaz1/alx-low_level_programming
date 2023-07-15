a6a5c0c29e2bd0f99b601d255fb36eafe4192b72
Osama <Osama.az1.os@gmail.com>
2023-07-15 02:12:34 -0700

    6

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry part
+ *
+ * Description: print alphabet lowercase.
+ *
+ * Return: always 0 (success).
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
