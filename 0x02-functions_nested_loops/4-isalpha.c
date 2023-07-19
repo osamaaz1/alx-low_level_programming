90269c1bb2ab5a65de6e56a36431981e97317e69
Osama <Osama.az1.os@gmail.com>
2023-07-19 13:10:41 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - Entry part
+ * @c: The character to be test
+ * Description: return 1 if it is a letter
+ *
+ * Return: 1 if c is a letter
+*/
+
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123 || c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
