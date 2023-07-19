e21c1225d0deea350238aeff53da4c816769baed
Osama <Osama.az1.os@gmail.com>
2023-07-19 13:39:46 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,17 +18,16 @@ void times_table(void)
 				{
 					_putchar(',');
 					_putchar('\t');
-					_putchar('\t');
+					if (product < 10)
+						_putchar('\t');
 				}
-				_putchar(product + '0');
 			}
-			else
-			{
-				_putchar(',');
-				_putchar('\t');
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
-			}
+			if (product < 10)
+				_putchar(' ');
+
+			if (product >= 10)
+				_putchar(product / 10 + 'O');
+			_putchar(product % 10 + 'O');
 		}
 	_putchar('\n');
 	}
