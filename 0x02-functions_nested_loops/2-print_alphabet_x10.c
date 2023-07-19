3f6cafb6f0fb1120ec392c83fb08479643bbe8d4
Osama <Osama.az1.os@gmail.com>
2023-07-19 12:25:47 -0700

    3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,9 @@
 void print_alphabet_x10(void)
 {
 	char letter = 'a';
+	int count;
 
-	for (int count = 0; count < 10; count++(
+	for (count = 0; count < 10; count ++(
 	{
 		while (letter <= 'z')
 		{
