e8902918eae81e62b9632dfd093e0396f4c5d2ac
Osama <Osama.az1.os@gmail.com>
2023-07-20 12:29:02 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,16 +14,14 @@ void print_square(int size)
 	if (size < 0)
 	{
 		_putchar('\n');
+		return;
 	}
-	else
+	for (row = 0; row < size; row++)
 	{
-		for (row = 0; row < size; row++)
+		for (col = 0; col < size; col++)
 		{
-			for (col = 0; col < size; col++)
-			{
-				_putchar('#');
-			}
-		_putchar('\n');
+			_putchar('#');
 		}
+	_putchar('\n');
 	}
 }
