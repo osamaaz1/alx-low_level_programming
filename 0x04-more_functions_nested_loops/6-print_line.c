e3d7206a1a5f5a91ed50d03f8f5288cc71fbb05e
Osama <Osama.az1.os@gmail.com>
2023-07-20 07:30:51 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print line - Entry part
+ * @ c: line distance
+ *
+ * Description: print streght line!
+ *
+ * Return: nothing!
+*/
+
+void more_numbers(int c)
+{
+	int dist;
+
+	for (dist = 0; dist < c; dist++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
