c42fb529c7ed18ad1fac6d69f141a89ee0806941
Osama <Osama.az1.os@gmail.com>
2023-07-20 06:16:11 -0700

    3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Entry part
+ * @a, @b: two numbers to mul.
+ * Description: return c the value of mul axb
+ *
+ * Return: return c value
+*/
+
+int mul(int a, int b)
+{
+	int c = a * b;
+	return (c);
+}
