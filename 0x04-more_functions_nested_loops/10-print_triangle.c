094f1cad6fc56f778b275748628f1a4aff7a16f1
Osama <Osama.az1.os@gmail.com>
2023-07-20 13:32:36 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,6 +7,7 @@
  *
  * Return: nothing!
 */
+
 void print_triangle(int size)
 {
 	if (size <= 0)
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,5 +13,4 @@ void print_diagonal(int n);
 void print_square(int size);
 int main(void);
 void print_triangle(int size);
-
 #endif /* MAIN_H */
