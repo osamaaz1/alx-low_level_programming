7a679ac6e1e974da81fc9c1ae9bb5edc55afa28f
Osama <Osama.az1.os@gmail.com>
2023-07-20 07:24:31 -0700

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - Entry part
+ *
+ * Description: print numbers from 0 to 14, 10 times
+ *
+ * Return: nothing!
+*/
+
+void more_numbers(void)
+{
+	int count, nums;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (nums = 0; nums < 15; nums++)
+		{
+			if (nums > 9)
+			{
+				_putchar((count / 10) + '0');
+			}
+			_putchar((count % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
