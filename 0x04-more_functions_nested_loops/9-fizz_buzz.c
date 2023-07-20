109072156ad4e44b7502719d0eb851f54088b0ca
Osama <Osama.az1.os@gmail.com>
2023-07-20 13:18:26 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,15 +9,18 @@
 int main(void)
 {
 	int x;
-	printf("0 ");
-	for (x = 1; x <= 100; x++)
+
+	for (x = 0; x <= 100; x++)
 	{
-		if (x % 3 == 0  && x % 5 == 0)
-			printf("FizzBuzz");
-		else if (x % 3 == 0)
-			printf("Fuzz");
-		else if (x % 5 == 0)
-			printf("Buzz");
+		if (x != 0)
+		{
+			if (x % 3 == 0  && x % 5 == 0)
+				printf("FizzBuzz");
+			else if (x % 3 == 0)
+				printf("Fuzz");
+			else if (x % 5 == 0)
+				printf("Buzz");
+		}
 		else
 			printf("%d", x);
 		if (x != 100)
