97d6d3b034ebf08bcea83ed65bcf763fb5c5b42b
Osama <Osama.az1.os@gmail.com>
2023-07-20 07:43:29 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
- * print line - Entry part
- * @ c: line distance
+ * print_line - Entry part
+ * @c: line distance
  *
  * Description: print streght line!
  *
  * Return: nothing!
 */
-
 void print_line(int c)
 {
 	int dist;
-
-	for (dist = 0; dist < c; dist++)
+	
+	if (c > 0)
 	{
-		_putchar(95);
+		for (dist = 0; dist < c; dist++)
+		{
+			_putchar(95);
+		}
 	}
 	_putchar('\n');
 }
