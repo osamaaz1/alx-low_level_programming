ac0c5905a345b6a179cbba79d4f9f417eefd738a
Osama <Osama.az1.os@gmail.com>
2023-07-20 06:47:40 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entry part
+ *
+ * Description: print numbers from 0 to 9 without 2 and 4
+ *
+ * Return: nothing!
+*/
+
+void print_most_numbers(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		if (count != 4 || count != 2)
+		{
+			_putchar(count + 48);
+		}
+	}
+	_putchar('\n');
+}
