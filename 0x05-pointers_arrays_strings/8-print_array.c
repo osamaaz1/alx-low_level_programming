e5ee70c7cf4d60a788e4022f470ada671dce4e7e
Osama <Osama.az1.os@gmail.com>
2023-07-24 06:18:08 -0700

    9

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: the array of integers
+ * @n: the number of elements to be printed
+ *
+ * Description: Print array
+ *
+ * Return : None
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
