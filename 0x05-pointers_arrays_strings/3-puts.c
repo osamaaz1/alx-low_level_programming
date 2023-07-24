c2fde902ba7c5784869a6a7dbd31d60c76b560f5
Osama <Osama.az1.os@gmail.com>
2023-07-24 04:20:20 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts - main function.
+ * @str : char that want to count the lenth.
+ *
+ * Description: to print a str with endline!
+ *
+ * Return: None.
+ */
+int _put(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
