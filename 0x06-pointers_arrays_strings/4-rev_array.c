46ae999f2796f01d051cc4fc70616dcc9840bb97
Osama <Osama.az1.os@gmail.com>
2023-07-26 03:07:52 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - main function.
+ * @s1 : first char
+ * @s2 : sec char
+ *
+ * Description: used to compare two strings.
+ *
+ * Return: compation between s1 and s2.
+ */
+void _reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n - 1; i >= 0; i--){
+		_putchar(a[i]);
+	}
+}
