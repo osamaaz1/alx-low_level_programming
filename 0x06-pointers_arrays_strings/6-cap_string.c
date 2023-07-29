67feda10f4ca1290040a0dcf0e16b80c0975fc3e
Osama <Osama.az1.os@gmail.com>
2023-07-29 02:10:16 -0700

    1

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,27 +12,27 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	if (str[0] >= 'a' && str[0] <= 'z')
-	{
-		str[0] -= 32;
-	}
+        while (str[i] != '\0')
+        {
 
-	while (str[i] != '\0')
-	{
+		if (str[0] >= 97 && str[0] <= 122)
+		{
+			str[0] -= 32;
+		}
 		if (str[i] == ' ' || str[i] == '\t' ||
 				str[i] == '\n' || str[i] == ',' ||
 				str[i] == ';' || str[i] == '.' ||
 				str[i] == '!' || str[i] == '?' ||
 				str[i] == '\"' || str[i] == '(' ||
 				str[i] == ')' || str[i] == '{' ||
-				str[i] == '}')
+				str[i] == '}' || str[i] == '"')
 		{
-			i++;
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] -= 32;
 			}
 		}
+
 	i++;
 	}
 	return (str);
