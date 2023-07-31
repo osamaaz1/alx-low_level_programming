17cd65284771691f8970c8c0ee34a89dee774a2d
Osama <Osama.az1.os@gmail.com>
2023-07-31 04:58:04 -0700

    Done!

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - main function.
+ * @s: from data
+ * @to: to data
+ * Description: function that sets the value of a pointer to a char.
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to);
+{
+	*s = to;
+}
