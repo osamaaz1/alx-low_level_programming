2b18cf9fc6894984891e543aae1d7ddc92b20d24
Osama <Osama.az1.os@gmail.com>
2023-07-31 05:56:55 -0700

    1

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,12 +11,14 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i;
+	unsigned int i, j;
 
 	for (i = 0;s[i] != '\0'; i++)
 	{
-		if (strchr (accept, s[i]) == 0){
-			break;
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
 		}
 	}
 	return (i);
