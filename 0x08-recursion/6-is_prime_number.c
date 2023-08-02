d213b3d5f5ec520857da219fcda98c6ff98b5b90
Osama <Osama.az1.os@gmail.com>
2023-08-02 03:36:19 -0700

    5

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,32 @@
 #include "main.h"
 
 /**
+ * _abs - main function
+ * @nm: number
+ *
+ * Description: function return the abs value.
+ *
+ * Return: abs number
+ */
+int _abs(int nm)
+{
+	return ((nm < 0) ? -nm : nm);
+}
+
+/**
  * is_prime_number - main function
  * @n: test number
  *
- * Description: function that returns 1 if the input integer is a prime else return 0.
+ * Description: function that returns
+ * 1 if the input integer is a prime else return 0.
  *
- * Return: 1 or 0.
+ * Return: 1 or 0
  */
+
 int is_prime_number(int n)
 {
-	if (n % 2 == 0 || n % 3 == 0 || n == 1 || n == 0 || n == -1)
+	n = _abs(n);
+	if (n % 2 == 0 || n % 3 == 0 || n == 1 || n == 0)
 		return (0);
 	return (1);
 }
