53519cb320f5d4388723d7588f71f21a54ac59ca
Osama <Osama.az1.os@gmail.com>
2023-08-02 02:28:55 -0700

    0

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - main function
+ * @x: int base
+ * @y: int power
+ *
+ * Description: function that returns the value of x raised to the power of y.
+ *
+ * Return: x^y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (x == 1)
+		return (1);
+	else if (y == 1)
+		return (x * _pow_recursion(x, y - 1));
+}
