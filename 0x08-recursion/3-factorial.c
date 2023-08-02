309e8773f7997427f694a5a84949570ed8629e8a
Osama <Osama.az1.os@gmail.com>
2023-08-02 01:38:04 -0700

    0

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - main function
+ * @n: int number to find fact
+ *
+ * Description: function that returns the factorial of a given number.
+ *
+ * Return: factorial of n.
+ */
+int factorial(int n);
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else
+		return (n * factorial(n - 1));
+}
