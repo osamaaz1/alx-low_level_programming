3fcef2a3db379fb20f9bab1da6d47c30421f28f1
Osama <Osama.az1.os@gmail.com>
2023-08-05 00:50:57 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function.
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Description: function to print the program name.
+ *
+ * Return: 0 if suc.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
