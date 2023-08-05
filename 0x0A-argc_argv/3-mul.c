7c7a75db9944509fecd88ab06bd1b02b7467be16
Osama <Osama.az1.os@gmail.com>
2023-08-05 01:23:32 -0700

    1

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function.
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Description: program should print the result of the multiplication.
+ *
+ * Return: 0 if succ.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int mul;
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
+}
