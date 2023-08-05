212c517f6e5ccc956410485420cbfbb736ab6469
Osama <Osama.az1.os@gmail.com>
2023-08-05 04:57:11 -0700

    ad

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry part
+ *
+ * Description: program that prints all possible
+ * different combinations of three digits.
+ *
+ * Return: always 0 (success).
+*/
+int main(void)
+{
+
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+			for (k = j; k < 58; k++)
+			{
+				if (i == j || j == k || i == k)
+				{
+					continue;
+				}
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i == 55 && j == 56 && k == 57)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
