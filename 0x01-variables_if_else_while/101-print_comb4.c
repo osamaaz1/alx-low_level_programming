55e12c40933f70cab1eda969883d363a399c982e
Osama <Osama.az1.os@gmail.com>
2023-08-05 04:55:04 -0700

    ad

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,29 +8,23 @@
  *
  * Return: always 0 (success).
 */
-
 int main(void)
 {
-	int i, j, k;
+	int digit1, digit2;
 
-	for (i = 0; i <= 7; i++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (j = i + 1; j <= 8; j++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			for (k = j + 1; k <= 9; k++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
-
-				if (i != 7 || j != 8 || k != 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
