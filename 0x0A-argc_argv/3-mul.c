c42eb21ec0cada64c49f3085a44cd9c0f94ae27e
Osama <Osama.az1.os@gmail.com>
2023-08-05 01:25:49 -0700

    1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,11 +17,14 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	int mul;
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	else
+	{
+		int mul;
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
 
-	mul = num1 * num2;
-	printf("%d\n", mul);
-	return (0);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+		return (0);
+	}
 }
