ed6c503d26beda8630514396251a8e47ada60d34
Osama <Osama.az1.os@gmail.com>
2023-08-08 05:05:23 -0700

    1

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - main function.
+ * @width: first string
+ * @height: sec string
+ *
+ * Description: function that concatenates two strings.,
+ *
+ * Return: Null if Failed, and string if succ
+ */
+int **alloc_grid(int width, int height)
+{ 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+    	int **grid = (int **)malloc(height * sizeof(int *));
+    	if (grid == NULL)
+	{
+		return (NULL);
+    	}
+    	int i, j;
+
+    	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+	    		for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
