73af226a347d379fcd9ff2cef9eaabacfefa35e3
Osama <Osama.az1.os@gmail.com>
2023-08-08 03:40:41 -0700

    1

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - main function.
+ * @s1: first string
+ * @s2: sec string
+ *
+ * Description: function that concatenates two strings.,
+ *
+ * Return: Null if Failed, and string if succ
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *constr;
+	size_t len1;
+	size_t len2;
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	constr = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	if (constr == NULL)
+		return (NULL);
+	strcat(constr, s1);
+	strcat(constr, s2);
+	return (constr);
+}
+
