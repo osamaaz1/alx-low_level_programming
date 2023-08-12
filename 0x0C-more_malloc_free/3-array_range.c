79611961003cf770296056cf68d0af0c4ea033da
Osama <Osama.az1.os@gmail.com>
2023-08-12 02:12:39 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 		return (NULL);
 
 	size = max - min + 1;
-	arr = malloc(sizeof(int) * ((max - min) + 1));
+	arr = malloc(sizeof(*arr) * ((max - min) + 1));
 	if (arr == NULL)
 		return (NULL);
 
