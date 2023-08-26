0c8c68cd9cfa7d9231956da241945c122b4a4adf
Osama <osama.az1.os@gmail.com>
2023-08-26 05:18:14 -0700

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,16 @@
-section .data
-    hello db "Hello, Holberton", 0
-    fmt db "%s", 0
-    nl db 10, 0
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-    global main
-    extern printf
-
+	SECTION .text
+	extern printf
+	global main
 main:
-    push rbp
-    mov rdi, fmt
-    mov rsi, hello
-    call printf
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-    mov rdi, fmt
-    mov rsi, nl
-    call printf
-
-    pop rbp
-    ret
+	mov eax, 0
+	ret
+	
