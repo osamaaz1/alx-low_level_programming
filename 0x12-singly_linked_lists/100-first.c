9e08b8a2bce161f7fde3b0b34023f80daf114781
Osama <osama.az1.os@gmail.com>
2023-08-26 05:15:45 -0700

    1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 
 /**
- * before_main - Function that runs before main.
- *
- * Description: This function is automatically executed before the main
- * function. It prints two lines of text to the standard output.
+ * bmain - function executed before main
+ * Return: no return.
  */
-void before_main(void) __attribute__((constructor));
 
-void before_main(void)
+void __attribute__ ((constructor)) bmain()
 {
-  printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-
-int main(void)
-{
-  return (0);
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
